6f9272ec80d16d40af3fa4083a6b4de44aa5b3f0
rezen <rezen.khadgi@gmail.com>
2025-12-30 22:23:49 +0545

    user dto added

---
+++ src/dtos/user.dto.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+import { UserSchema } from "../types/user.type";
+
+export const RegisterUserDto = UserSchema.pick({
+  fullname: true,
+  email: true,
+  password: true,
+  confirmPassword: true,
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Password do not match",
+  path: ["confirmPassword"],
+});
+
+export type RegisterUserDto = z.infer<typeof RegisterUserDto>;
+
+export const LoginUserDto = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+});
+
+export type LoginUserDto = z.infer<typeof LoginUserDto>;
+
+export const UpdateUserDto = UserSchema.pick({
+  fullname: true,
+  email: true,
+}).partial();
+export type UpdateUserDto = z.infer<typeof UpdateUserDto>;
---
+++ src/types/user.type.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const UserSchema = z.object({
+  fullname: z.string().min(2),
+  email: z.email(),
+  role: z.enum(["user", "organization", "admin"]).default("user"),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+});
+
+export type UserType = z.infer<typeof UserSchema>;
