9a833d4a3004e3a97c5d71a3dc8f4ddec7b9210e
rezen <rezen.khadgi@gmail.com>
2026-01-31 12:21:51 +0545

    admin dto created

--- src/dtos/admin.dto.ts
+++ src/dtos/admin.dto.ts
@@ -1,22 +1,22 @@
-// import z from "zod";
-// import { UserSchema } from "../types/user.type";
+import z from "zod";
+import { UserSchema } from "../types/user.type";
 
-// export const CreateNewUserDto = UserSchema.pick({
-//   fullName: true,
-//   email: true,
-//   phoneNumber: true,
-//   password: true,
-//   confirmPassword: true,
-// }).refine((data) => data.password === data.confirmPassword, {
-//   message: "Password do not match",
-//   path: ["confirmPassword"],
-// });
+export const CreateNewUserDto = UserSchema.pick({
+  fullName: true,
+  email: true,
+  phoneNumber: true,
+  password: true,
+  confirmPassword: true,
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Password do not match",
+  path: ["confirmPassword"],
+});
 
-// export type CreateNewUserDto = z.infer<typeof CreateNewUserDto>;
+export type CreateNewUserDto = z.infer<typeof CreateNewUserDto>;
 
-// export const UpdateUserDto = UserSchema.pick({
-//   fullName: true,
-//   phoneNumber: true,
-//   profilePicture: true,
-// }).partial();
-// export type UpdateUserDto = z.infer<typeof UpdateUserDto>;
+export const UpdateUserDto = UserSchema.pick({
+  fullName: true,
+  phoneNumber: true,
+  profilePicture: true,
+}).partial();
+export type UpdateUserDto = z.infer<typeof UpdateUserDto>;
--- src/dtos/organization.dto.ts
+++ src/dtos/organization.dto.ts
@@ -1,15 +1,15 @@
-// import z from "zod";
-// import { UserSchema } from "../types/user.type";
+import z from "zod";
+import { UserSchema } from "../types/user.type";
 
-// export const CreateOrganizationDto = UserSchema.pick({
-//   fullName: true,
-//   email: true,
-//   phoneNumber: true,
-//   password: true,
-//   confirmPassword: true,
-// }).refine((data) => data.password === data.confirmPassword, {
-//   message: "Password do not match",
-//   path: ["confirmPassword"],
-// });
+export const CreateOrganizationDto = UserSchema.pick({
+  fullName: true,
+  email: true,
+  phoneNumber: true,
+  password: true,
+  confirmPassword: true,
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Password do not match",
+  path: ["confirmPassword"],
+});
 
-// export type CreateOrganizationDto = z.infer<typeof CreateOrganizationDto>;
+export type CreateOrganizationDto = z.infer<typeof CreateOrganizationDto>;
